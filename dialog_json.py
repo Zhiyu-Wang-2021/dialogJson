e04ad6dcd3eeab3140df47b49f412900426aff61
ZhiyuWang <zcabanq@uc.ac.uk>
2022-12-31 00:27:54 +0000

    add multi-condition dialog

--- dialog_json.py
+++ dialog_json.py
@@ -102,22 +102,209 @@ def entity(name, values, fuzzy_match=True):
 #     "previous_sibling": "Welcome"
 # }, ...],
 def dialog_nodes():
-    result = [
+    result_raw = [
         welcome_node(),
-        dialog_node(
-            "Location Info", "You are here", "#location_info", "Welcome"
+        dialog_node_multi_condition(
+            "Location Info",
+            {
+                "handler": "@uk_location",
+                "enter": "#location_info",
+                "output": [
+                    ("true", "Our store is at ..."),
+                    ("$location", "Our $location store is at ...")
+                ]
+            },
+            "$location",
+            "Welcome"
         ),
-        dialog_node(
+        dialog_node_standard(
             "Hours Info", "It's time", "#hours_info", "Location Info"
         ),
         else_node("Hours Info")
     ]
+    result = []
+    for r in result_raw:
+        if isinstance(r, list):
+            result.extend(r)
+        else:
+            result.append(r)
     return '"dialog_nodes": ' + json.dumps(result) + ','
 
 
-def dialog_node(reference, output, condition, previous_sibling, node_type='standard'):
+# multiple responds node
+# handler -> slot
+# frame
+# response_condition
+# {
+#     "type": "event_handler",
+#     "output": {},
+#     "parent": "slot_handler_parent",
+#     "event_name": "focus",
+#     "dialog_node": "handler_focus",
+#     "previous_sibling": "handler_uk_location"
+# },
+# {
+#     "type": "event_handler",
+#     "output": {},
+#     "parent": "slot_handler_parent",
+#     "context": {
+#         "location": "@uk_location"
+#     },
+#     "conditions": "@uk_location",
+#     "event_name": "input",
+#     "dialog_node": "handler_uk_location"
+# },
+# {
+#     "type": "slot",
+#     "parent": "Location Info",
+#     "variable": "$location",
+#     "dialog_node": "slot_handler_parent",
+#     "previous_sibling": "response_without_location"
+# },
+# {
+#     "type": "frame",
+#     "title": "Location Info",
+#     "metadata": {
+#         "_customization": {
+#             "mcr": true
+#         }
+#     },
+#     "conditions": "#location_info",
+#     "dialog_node": "Location Info",
+#     "previous_sibling": "Welcome"
+# },
+# {
+#     "type": "response_condition",
+#     "output": {
+#         "generic": [
+#             {
+#                 "values": [
+#                     {
+#                         "text": "Our store is at ..."
+#                     }
+#                 ],
+#                 "response_type": "text",
+#                 "selection_policy": "sequential"
+#             }
+#         ]
+#     },
+#     "parent": "Location Info",
+#     "conditions": "true",
+#     "dialog_node": "response_without_location",
+#     "previous_sibling": "response_with_location"
+# },
+# {
+#     "type": "response_condition",
+#     "output": {
+#         "generic": [
+#             {
+#                 "values": [
+#                     {
+#                         "text": "Our $location store is at ..."
+#                     }
+#                 ],
+#                 "response_type": "text",
+#                 "selection_policy": "sequential"
+#             }
+#         ]
+#     },
+#     "parent": "Location Info",
+#     "conditions": "$location",
+#     "dialog_node": "response_with_location"
+# }
+def dialog_node_multi_condition(reference, condition_dict, slot_var, previous_sibling):
+
+    handler_condition = condition_dict['handler']
+    enter_condition = condition_dict['enter']
+    outputs_and_conditions = condition_dict['output']
+
+    def handler():
+        return [
+            {
+                "type": "event_handler",
+                "output": {},
+                "parent": "slot_handler_parent_" + reference,
+                "event_name": "focus",
+                "dialog_node": "handler_focus_" + reference,
+                "previous_sibling": "handler_" + reference
+            },
+            {
+                "type": "event_handler",
+                "output": {},
+                "parent": "slot_handler_parent_" + reference,
+                "context": {
+                    "location": handler_condition
+                },
+                "conditions": handler_condition,
+                "event_name": "input",
+                "dialog_node": "handler_" + reference
+            }
+        ]
+
+    def slot():
+        return {
+                "type": "slot",
+                "parent": reference,
+                "variable": slot_var,
+                "dialog_node": "slot_handler_parent_" + reference,
+                "previous_sibling": "response_" + reference + "0"
+            }
+
+    def frame():
+        return {
+                   "type": "frame",
+                   "title": reference,
+                   "metadata": {
+                       "_customization": {
+                           "mcr": True
+                       }
+                   },
+                   "conditions": enter_condition,
+                   "dialog_node": reference,
+                   "previous_sibling": previous_sibling
+               }
+
+    def responses():
+        responses_list = []
+        node_idx = 0
+        for condition, output in outputs_and_conditions:
+            responses_list.append({
+                "type": "response_condition",
+                "output": {
+                    "generic": [
+                        {
+                            "values": [
+                                {
+                                    "text": output
+                                }
+                            ],
+                            "response_type": "text",
+                            "selection_policy": "sequential"
+                        }
+                    ]
+                },
+                "parent": reference,
+                "conditions": condition,
+                "dialog_node": "response_" + reference + str(node_idx)
+            })
+            if node_idx < len(outputs_and_conditions) - 1:
+                responses_list[node_idx]["previous_sibling"] = "response_" + reference + str(node_idx + 1)
+            node_idx += 1
+        return responses_list
+
+    result_raw = [handler(), slot(), frame(), responses()]
+    result = []
+    for r in result_raw:
+        if isinstance(r, list):
+            result.extend(r)
+        else:
+            result.append(r)
+    return result
+
+
+def dialog_node_standard(reference, output, condition, previous_sibling):
     return {
-        "type": node_type,
+        "type": 'standard',
         "title": reference,
         "output": {
             "generic": [
--- test.py
+++ test.py
@@ -2,6 +2,4 @@ from dialog_json import *
 from utils import *
 from data import *
 
-example = read_examples_from_json(IntentExamples.location_info)
-
-print(example)
+print(dialog_nodes())
